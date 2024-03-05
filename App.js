c3b542701e9d97292ca733c4f3dbebc373e5ae49
madinasidiqi <154350421+madinasidiqi@users.noreply.github.com>
2024-03-04 20:13:05 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,83 @@
+
+// //  first question...............
+
+// let mycountry ={
+//     country:"afg",
+//     capital:"kabul",
+//     language:"dari",
+//     population:40,
+//     neighbours:["india","iran","pakistan"],
+//     describe: function(){
+//         console.log(this.country+"has"+this.population+"melion finish speaking people"+this.neighbours+"neighbours in country and captal called "+this.capital);
+//     },
+//     checkIsland: function(){
+//         this.island= this.neighbours.length===0 ? true : false;
+//     }
+// }
+// //  question 2...............
+
+// // console.log(mycountry.country+"has"+mycountry.population+"melion finish speaking people"+mycountry.neighbours+"neighbours in country and captal called "+mycountry.capital);
+// // mycountry[population]=2;
+// mycountry.population=2;
+// console.log(mycountry.population);
+
+// //  question 3...............
+
+//  mycountry.describe();
+//  mycountry.checkIsland();
+//  console.log(mycountry);
+
+// //  question 4...............
+
+// for( let i =1 ; i<=50 ; i++){
+//     console.log("for numbers" + i + "is currently voting");
+// }
+
+// //  question 5...............
+
+// let percentage2 =[40 ,60,90,20];
+// function percentageOfWorld1(percentage2){
+//     return (Number(percentage2) * 100) / 7900 ;}
+
+// for(let i = 0; i < percentage2.length; i++)
+// {
+//  console.log(percentageOfWorld1(percentage2[i]))
+// }
+
+
+// //  question 6...............
+
+
+// let listofNeighbours =[['canada','mexico'],['spain'],['norway','sweden','russia']];
+// for(let i =0 ; i<listofNeighbours.length; i++){
+    
+
+// }
+
+// -------------------------------------------------------
+
+// // loop section 
+// question 7...............
+//  let percentage3 =[40 ,60,90,20];
+
+// function percentageOfWorld1(percentage3){
+//     return (Number(percentage3) * 100) / 7900 ;}
+
+//     let i = 0;
+//     while(i < percentage3.length)
+//     {
+//         console.log(percentageOfWorld1(percentage3[i]));
+//         i++
+//     }
+
+// the for loop is easier for me 
+
+// lecture Iteration , The for loop 
+
+// let voters = 50;
+// for(i = 1 ; i<=voters; i++)
+// {
+//   console.log("Voter number "+ i + " is currently voting")
+// }
+
+
